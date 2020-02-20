5d8b35227b79362a32bf44f10e8254910750fc7b
Isaac Iniongun <iniongunisaac@gmail.com>
2020-02-20 20:24:55 +0100

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Isaac Iniongun on 20/02/2020.
+//  Copyright © 2020 Iniongun Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
